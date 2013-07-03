241f0095a7c35cb04735971b9f60e329af571fd2
harshadshirwadkar <harshadshirwadkar@gmail.com>
2013-07-03 19:21:54 +0530

    Integrated the call to the binary into the script.

--- run_sifs_calc.sh
+++ run_sifs_calc.sh
@@ -1,13 +1,56 @@
+
 #!/bin/bash
 
 PCAP_FILE=$1
-HEADER_TYPE=$2
 TMP_CSV_FILE=/tmp/sifs_calc.csv
 
-if [ "$HEADER_TYPE" = "PRISM" ] ; then
+calculate_sifs()
+{
+    if [ "$2" = "prism" ]; then
+	proto=p
+    else
+	proto=r
+    fi
+    ./sifs_calculator -i $1 -p $proto
+    rm $TMP_CSV_FILE
+}
+
+
+if [ "$1" = "" ]; then
+    echo "Usage: $0 <pcap_file>"
+    exit
+fi
+
+# Check if tshark is installed
+hash tshark 2>/dev/null
+if [ "$?" != "0" ]; then
+    echo "tshark is not installed. Run the following command to install it:"
+    echo "sudo apt-get install tshark"
+    exit
+fi
+
+HEADER_TYPE=`tshark -r sample_trace.pcap -T fields -e frame.number -e frame.protocols frame.number==1 -E separator="," | cut -d "," -f2 | cut -d ":" -f1`
+
+echo -ne "Exporting to csv ... "
+if [ "$HEADER_TYPE" = "prism" ] ; then
     tshark -r $PCAP_FILE -T fields -e wlan.fc.type_subtype -e prism.did.hosttime -e prism.did.rate -e prism.did.frmlen -e wlan.da -e wlan.bssid -e wlan.sa -e wlan.ra -E separator=',' > $TMP_CSV_FILE
-elif [ "$HEADER_TYPE" = "RADIOTAP" ] ; then
+    if [ "$?" = "0" ] ; then
+	echo "[DONE]"
+	calculate_sifs $TMP_CSV_FILE $HEADER_TYPE
+	exit
+    else
+	echo "pcap can't be exported to CSV."
+	exit
+    fi
+elif [ "$HEADER_TYPE" = "radiotap" ] ; then
     tshark -r $PCAP_FILE -T fields -e wlan.fc.type_subtype -e "dummy" -e radiotap.mactime -e "dummy" -e radiotap.datarate -e frame.len -e wlan.da -e wlan.bssid -e wlan.sa -e wlan.ra -E separator=',' > $TMP_CSV_FILE
+    if [ "$?" = "0" ] ; then
+	echo "Successfully created CSV file : $TMP_CSV_FILE"
+	exit
+    else
+	echo "Failure while creating CSV file"
+	exit
+    fi
 else
-    echo "Error"
+    echo "Unrecognized header type"
 fi
--- sifs_calculator.c
+++ sifs_calculator.c
@@ -17,223 +17,184 @@ typedef char macaddr[STR_MAC_ADDR_LEN];
 #define BROADCAST_MAC "FF:FF:FF:FF:FF:FF"
 #define STRING_EQUAL(_str1, _str2, _no) (strncasecmp((_str1), (_str2), (_no)) == 0)
 
+#define IS_OUTLIER(packet_gap) (((packet_gap) > 100) || ((packet_gap) < 0))
+
+
 struct packet {
-    int type, rate, frmlen, number;
-    long long timestamp;
-    macaddr dst, src, bssid;
+	int type, rate, frmlen, number;
+	long long timestamp;
+	macaddr dst, src, bssid;
 };
 
-int is_prism;
-
-void usage(void)
-{
-    printf("usage\n");
-    return;
-}
+int proto;
 
 void parse_line(char *line, struct packet *packet)
 {
-    int i=0, pass=0, read_val = 1;
-    char *cur, *ptr;
+	int i=0, pass=0, read_val = 1;
+	char *cur, *ptr;
 
-    for(cur = line; (cur - line) < 127;) {
-	if(*cur == ',') {
-	    cur++;
-	    read_val = 1;
-	    if(*cur == ',')
-		read_val = 0;
-	}
-	if(*cur == '\n')
-	    break;
-	if(read_val) {
-	    switch(pass) {
-	    case 0:
-		/* Type */
-		packet->type = (int)strtol(cur, NULL, 0);
-		break;
-	    case 2:
-		/* Timestamp */
-		packet->timestamp = (long long)strtoll(cur, NULL, 0);
-		break;
-	    case 4:
-		if(is_prism)
-		    packet->rate = (int)strtol(cur, NULL, 0);
+	for(cur = line; (cur - line) < 127;) {
+		if(*cur == ',') {
+			cur++;
+			read_val = 1;
+			if(*cur == ',')
+				read_val = 0;
+		}
+		if(*cur == '\n')
+			break;
+		if(read_val) {
+			switch(pass) {
+			case 0:
+				/* Type */
+				packet->type = (int)strtol(cur, NULL, 0);
+				break;
+			case 2:
+				/* Timestamp */
+				packet->timestamp = (long long)strtoll(cur, NULL, 0);
+				break;
+			case 4:
+				if(proto == 'p')
+					packet->rate = (int)strtol(cur, NULL, 0);
+				else
+					packet->rate = ((float)strtof(cur, NULL) * 2.0);
+				break;
+			case 5:
+				/* frmlen */
+				packet->frmlen = (int)strtol(cur, NULL, 0);
+				break;
+			case 6:
+				/* dst */
+				strncpy(packet->dst, cur, 17);
+				packet->dst[17] = 0;
+				break;
+			case 7:
+				/* bssid */
+				strncpy(packet->bssid, cur, 17);
+				packet->bssid[17] = 0;
+				break;
+			case 8:
+				/* src */
+				strncpy(packet->src, cur, 17);
+				packet->src[17] = 0;
+				break;
+			case 9:
+				/* receiver address in case of RTS/CTS/ACK */
+				strncpy(packet->dst, cur, 17);
+				packet->dst[17] = 0;
+				break;
+			default: break;
+			}
+			read_val = 0;
+		}
+		pass++;
+		ptr = strchr(cur, ',');
+		if(ptr)
+			cur = ptr;
 		else
-		    packet->rate = ((float)strtof(cur, NULL) * 2.0);
-		break;
-	    case 5:
-		/* frmlen */
-		packet->frmlen = (int)strtol(cur, NULL, 0);
-		break;
-	    case 6:
-		/* dst */
-		strncpy(packet->dst, cur, 17);
-		packet->dst[17] = 0;
-		break;
-	    case 7:
-		/* bssid */
-		strncpy(packet->bssid, cur, 17);
-		packet->bssid[17] = 0;
-		break;
-	    case 8:
-		/* src */
-		strncpy(packet->src, cur, 17);
-		packet->src[17] = 0;
-		break;
-	    case 9:
-		/* receiver address in case of RTS/CTS/ACK */
-		strncpy(packet->dst, cur, 17);
-		packet->dst[17] = 0;
-		break;
-	    default: break;
-	    }
-	    read_val = 0;
+			break;
 	}
-	pass++;
-	ptr = strchr(cur, ',');
-	if(ptr)
-	    cur = ptr;
-	else
-	    break;
-    }
 }
 
 inline long long time_for(long frmlen, int rate)
 {
-    return (long long)(((frmlen * 8) * 1024.0 * 1024.0)/(((double)rate/2) * 1000.0 * 1000.0));
+	return (long long)(((frmlen * 8) * 1024.0 * 1024.0)/(((double)rate/2) * 1000.0 * 1000.0));
 }
 
 inline int is_ackable(int type)
 {
-    switch(type) {
-    case IEEE80211_ACK:
-    case IEEE80211_CTS:
-    case IEEE80211_RTS:
-	return 0;
-    }
-    return 1;
+	switch(type) {
+	case IEEE80211_ACK:
+	case IEEE80211_CTS:
+	case IEEE80211_RTS:
+		return 0;
+	}
+	return 1;
 }
 
 void dump_packet(struct packet *packet)
 {
-    printf("%d)\t%x\t%d\t%d\t%lld\t%s\t%s\t%s\n", packet->number, packet->type,
-	   packet->rate, packet->frmlen, packet->timestamp,
-	   packet->dst, packet->src, packet->bssid);
+	printf("%d)\t%x\t%d\t%d\t%lld\t%s\t%s\t%s\n", packet->number, packet->type,
+	       packet->rate, packet->frmlen, packet->timestamp,
+	       packet->dst, packet->src, packet->bssid);
 }
 
-void calculate_sifs(char *input_file, macaddr mac, macaddr bssid)
+void calculate_sifs(char *input_file)
 {
-    FILE *fp = NULL;
-    char line[128];
-    macaddr oldmac={0};
-    struct packet old_packet;
-    int packet_number, old_packet_acked;
+	FILE *fp = NULL;
+	char line[128];
+	macaddr oldmac={0};
+	struct packet old_packet;
+	int packet_number, old_packet_acked;
+	long long timestamp, timestamp_old = 0;
+	long long ack_count = 0;
+	long double sifs = 0.0;
 
-    fp = fopen(input_file, "r+");
-    if(!fp) {
-	printf("Couldn't open input file");
-	exit(EXIT_FAILURE);
-    }
-
-    packet_number = 0;
-    memset(&old_packet, 0, sizeof(struct packet));
-    while(!feof(fp)) {
-	struct packet packet;
-
-	fgets(line, 128, fp);	
-	memset(&packet, 0, sizeof(struct packet));
-
-	parse_line(line, &packet);
-	packet.number = ++packet_number;
-
-	if(STRING_EQUAL(BROADCAST_MAC, packet.dst, 17))
-	    continue;
-	if(packet.type == IEEE80211_DATA && !STRING_EQUAL(bssid, packet.bssid, 17))
-	    continue;
-	if(packet.type != IEEE80211_ACK && !STRING_EQUAL(packet.src, mac, 17)
-	   && !STRING_EQUAL(packet.dst, mac, 17))
-	    continue;
-
-	if(packet.type == IEEE80211_ACK) {
-	    if(STRING_EQUAL(old_packet.src, packet.dst, 17) ||
-	       STRING_EQUAL(old_packet.bssid, packet.dst, 17)) {
-		long long timestamp_ack, timestamp_old;
-		//		printf("%d: Ack RECEIVER %s - Acks packet number %d (%d)--->\n", packet_number, packet.dst, old_packet.number, time_for(packet.frmlen, packet.rate));
-		timestamp_ack = packet.timestamp - time_for(packet.frmlen, packet.rate) - PLCP_TIME;
-		timestamp_old = old_packet.timestamp;
-		if(STRING_EQUAL(old_packet.dst, mac, 17))
-		    printf("%lld\n", timestamp_ack - timestamp_old);
-		//		printf("\t%d: DST %s BSSID %s SRC %s\n", old_packet.number, old_packet.dst, old_packet.bssid, old_packet.src);
-		//		printf("\t\tReceived within %ldus\n\n", timestamp_ack - timestamp_old);
-		memset(&old_packet, 0, sizeof(struct packet));
-	    }
-	    /* else  */
-	    /* 	printf("%d: Unresolved ack packet dst %s\n\n", packet_number, packet.dst); */
+	fp = fopen(input_file, "r+");
+	if(!fp) {
+		printf("Couldn't open input file");
+		exit(EXIT_FAILURE);
 	}
-	else {
-	    if(is_ackable(packet.type))
+
+	packet_number = 0;
+	memset(&old_packet, 0, sizeof(struct packet));
+	while(!feof(fp)) {
+		struct packet packet;
+
+		fgets(line, 128, fp);	
+		memset(&packet, 0, sizeof(struct packet));
+
+		parse_line(line, &packet);
+		packet.number = ++packet_number;
+
+		timestamp = packet.timestamp - time_for(packet.frmlen, packet.rate);
+		timestamp_old = old_packet.timestamp;
+		//		printf("%d) Size %d travelling @%dmbps took %lld time(Interpacket gap = %lld)\n", packet.number, packet.frmlen, packet.rate, time_for(packet.frmlen, packet.rate) + PLCP_TIME, timestamp - timestamp_old);
+		if((packet.type == 0x1d) &&
+		   !IS_OUTLIER(timestamp - timestamp_old)) {
+			sifs = ((((sifs) * ack_count) + (timestamp - timestamp_old)) / (++ack_count));
+		}
 		old_packet = packet;
 	}
-    }
+
+	printf("Avergage SIFS observed in the trace = %Lfus\n", sifs);
+	printf("\tTotal ack frames = %lld\n", ack_count);
 }
 
 int is_valid(char *str)
 {
-    if(str[0] == 0)
-	return 0;
-    return 1;
+	if(str[0] == 0)
+		return 0;
+	return 1;
 }
 
-void read_clients(char *input_file)
+void usage(void)
 {
-    FILE *fp = NULL;
-    char line[128];
-
-    fp = fopen(input_file, "r+");
-    if(!fp) {
-	printf("Couldn't open input file");
-	exit(EXIT_FAILURE);
-    }
-    while(!feof(fp)) {
-	struct packet packet;
-
-	fgets(line, 128, fp);	
-	memset(&packet, 0, sizeof(struct packet));
-
-	parse_line(line, &packet);
-	
-    }
+	printf("Usage: -i <input_file> -p <protocol p:prism r:radiotap>\n");
 }
 
 int main(int argc, char *argv[])
 {
-    char c;
-    macaddr mac={0}, bssid={0};
-    char input_file[MAX_FILENAME] = {0};
+	char c;
+	char input_file[MAX_FILENAME] = {0};
 
-    is_prism = 0;
-    while((c = getopt(argc, argv, "i:b:m:p")) != -1) {
-	switch(c) {
-	case 'i':
-	    strcpy(input_file, optarg);
-	    break;
-	case 'b':
-	    strcpy(bssid, optarg);
-	    break;
-	case 'm':
-	    strcpy(mac, optarg);
-	    break;
-	case 'p':
-	    is_prism = 1;
-	    break;
+	proto = 0;
+	while((c = getopt(argc, argv, "i:p:")) != -1) {
+		switch(c) {
+		case 'i':
+			strcpy(input_file, optarg);
+			break;
+		case 'p':
+			proto = (int)optarg;
+			break;
+		default:
+			usage();
+			return 0;
+		}
 	}
-    }
-    if(!is_valid(input_file))
+	if((!is_valid(input_file)) || (proto == 0)) {
+		usage();
+		return 0;
+	}
+	calculate_sifs(input_file);
 	return 0;
-    if(!is_valid(bssid) || !is_valid(mac)) {
-	read_clients(input_file);
-	return 0;
-    }
-    printf("mac - %s\t bssid - %s\n", mac, bssid);
-    calculate_sifs(input_file, mac, bssid);
 }
